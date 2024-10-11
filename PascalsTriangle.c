cb848967f660524044d51386b4a89b61bcc40226
LTC-117 <lucastentoni@gmail.com>
2024-10-11 20:40:06 -0300

    Fixing issues

--- PascalsTriangle.c
+++ PascalsTriangle.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Variables:
-int num;
+//New type definition for llu
+typedef unsigned long long ullong;
 
-//Functions:
-long long int fact(int a){ //Factorial function.
+//Factorial function:
+ullong fact(int a){
     if(a == 0){
         return 1;
     }
 
-    long long int result = 1;
+    ullong result = 1;
     for(int i=1; i<=a; i++){
         result *= i;
     }
-    
+
     return result;
 }
 
 int main(){
-    int **face = (int **)malloc(21 * sizeof (int *));
-        for(int i=0; i<21; i++){
-            face[i] = (int *)malloc((i+1) * sizeof (int));
+    ullong **pascals_triangle = malloc(21 * sizeof (ullong *));
+    if(pascals_triangle == NULL){
+        fprintf(stderr, "ERROR ALOCATING MEMORY!");
+        return 1;
+    }
+
+    for(int i=0; i<21; i++){
+        pascals_triangle[i] = malloc((i+1) * sizeof (ullong));
+        if(pascals_triangle[i] == NULL){
+            fprintf(stderr, "ERROR ALOCATING MEMORY!");
+            return 1;
         }
+    }
 
     for(int i=0; i<21; i++){
         for (int j=0; j<=i; j++){
-            face[i][j] = fact(i)/(fact(j)*fact(i-j)); //Combination 'C(i, j)', that defines each element of the triangle.
-            printf(" %d", face[i][j]);
+            pascals_triangle[i][j] = fact(i)/(fact(j)*fact(i-j));
+            printf(" %llu", pascals_triangle[i][j]);
         }
         printf("\n");
     }
 
     for(int i=0; i<21; i++){
-        free(face[i]);
+        free(pascals_triangle[i]);
     }
 
-    free(face);
+    free(pascals_triangle);
 
     return 0;
 }
